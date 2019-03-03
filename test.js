5e4eff4f97111b7305e2ae52ca3ab7bdbf47ffd9
Thomas Cannon <tcannon00@gmail.com>
2019-03-02 21:24:00 -0700

    Experimenting with "dynamically loaded" popovers

--- test.js
+++ test.js
@@ -1,1 +1,14 @@
-import 'details-dialog-element'
+document.addEventListener('DOMContentLoaded', function(){
+  var dynamicPopup = document.querySelector('.dynamically-loaded')
+  dynamicPopup.addEventListener('toggle', function(event){
+    var popupBody = event.target.getElementsByTagName('details-popover')[0]
+    if(event.target.open){
+      event.target.classList.add('loading')
+      setTimeout(function(){
+        event.target.classList.remove('loading')
+        popupBody.innerHTML = "<h1>My new Body!</h1><p>Look at this!</p>"
+        console.log("weiuhfiuhwef")
+      }, 500)
+    }
+  })
+})
